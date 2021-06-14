75bcf0a2fd4332bd2e269a06723d1c7b12382610
Jan Häusler <jan.haeusler@metropolitan-cities.com>
2021-06-14 15:56:53 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,19 +3,25 @@
  * inside of the platform.
  */
 declare interface Platform {
-  deployConnector: (connector: ConnectorDescription) => void;
+  deployConnector: (
+    connector: ConnectorDescription
+  ) => void;
   deployTimer: (...argumentList: any[]) => void;
   addEntity: (id: string, state: any) => void;
-  store: (key: string, value: any) => Promise<void>;
+  store: (key: string, value: any) => void;
   get: (key: string) => Promise<any>;
   log: (...whatToLog: any[]) => void;
-  entity: (id: string) => Promise<Entity>;
+  entity: (id: string) => EntityCursor;
+  findEntities: (restrictions: EntityRestrictions) => Promise<any[]>;
+  func: (name: string) => any;
   sendConnectorEvent: (
     connectorId: number,
     type: string,
     payload: any
   ) => Promise<void>;
-  call: (functionCall: APIFunctionCallDescription) => Promise<any>;
+  call: (
+    functionCall: APIFunctionCallDescription
+  ) => Promise<any>;
   insertState: (
     entityId: string,
     property: string,
@@ -23,6 +29,9 @@ declare interface Platform {
     time?: number
   ) => Promise<any>;
   inquire: Function;
+  makeEntity: (entity: Entity, time: number) => Promise<void>;
+
+  init: Function;
 }
 
 interface ConnectorDescription {
@@ -37,18 +46,49 @@ type APIFunctionCallDescription = {
   parameters?: any;
 };
 
+type EntityStateValue = any;
+
 interface EntityState {
   time: number; // in UTC
-  value: any;
+  value: EntityStateValue;
   type?: string; // TODO: make this an enum
 }
 
+function instanceOfEntityState(object: any): object is EntityState {
+  return typeof object === 'object' && 'time' in object && 'value' in object;
+}
+
 interface EntityStateDictionary {
-  [key: string]: EntityState;
+  [key: string]: EntityState | EntityStateValue;
 }
 
 interface Entity {
-  id: string | number;
+  id?: string | number;
+  providedby?: number;
+  pid?: string | number;
+  type?: string;
+  methods?: any;
   name: string;
   state: EntityStateDictionary;
+}
+
+interface EntityRestrictions {
+  type?: String;
+  parameters?: Object;
+}
+
+interface EntityHistoryRestrictions {
+  from?: Number;
+  to?: Number;
+  property?: String;
+}
+
+interface EntityCursor {
+  history(restrictions: EntityHistoryRestrictions): Promise<any>;
+  state: (property: string) => {
+    from: (startDateTime: string | number) => {
+      to: (endDateTime: string | number) => Promise<any>,
+    },
+    at: (dateTime: string | number) => Promise<any>;
+  }
 }
